667dd8a2a55d50861a33d6732320c98f7a43277a
Ojas <ojaskhamkar@gmail.com>
2021-06-06 19:26:05 +0530

    First Java Code

---
+++ src/typeCasting/TypeCasting.java
@@ -0,0 +1,11 @@
+package typeCasting;
+
+public class TypeCasting {
+    public static void main(String[] args){
+        int x = 56648;
+        double v = 45.6399725138;
+        x = (int)v; // manual typecasting
+
+        System.out.println(x);
+    }
+}
